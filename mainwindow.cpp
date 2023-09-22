eaca0434180236bc687d270f1921e8f3f9653850
MarianoBenitti <benittimariano@hotmail.com>
2023-09-21 21:59:12 -0300

    Pelota finalizada comienzo de comunicacion

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -17,8 +17,29 @@ MainWindow::MainWindow(QWidget *parent)
     TimerGen = new QTimer(this);
     connect(TimerGen, &QTimer::timeout,this,&MainWindow::TimerGen1);
     TimerGen->start(10);
+    QSerialPort1= new QSerialPort(this);
+    connect(QSerialPort1,&QSerialPort::readyRead,this,&MainWindow::OnRxQSerialPort1);
+
+    //INICIALIZAMOS BUFFER DE ENTRADA
+    datosLec.iRL=0;
+    datosLec.iRE=0;
+    datosLec.nBytes=0;
+    datosLec.iDatos=0;
+    datosLec.checksum=0;
+    datosLec.estDecode=EST_U;
+    datosLec.tamBuffer=255;
+    datosLec.bufL=bufferL;
+    datosLec.iChecksum=0;
+    //INICIALIZAOS EL BUFFER DE SALIDA
+    datosEsc.iTL=0;
+    datosEsc.iTE=0;
+    datosEsc.checksum=0;
+    datosEsc.bufE=bufferE;
+    datosEsc.tamBuffer=255;
+
+
     s_pelotita.InMove=0;
-    s_pelotita.alfha=10;
+    s_pelotita.alfha=20;
     s_pelotita.anguloI=-40;
     s_pelotita.escMetro=100;
     s_pelotita.radio=10;
@@ -60,7 +81,7 @@ void MainWindow::TimerGen1(){
         if(ui->CheckTrayectoria->isChecked()){
 
                 pen.setColor(Qt::white);
-                brush.setColor(Qt::green);
+                brush.setColor(Qt::cyan);
                 QPainterEsp.setPen(pen);
                 QPainterEsp.setBrush(brush);
                 QPainterEsp.drawEllipse(s_pelotita.centro,s_pelotita.radio,s_pelotita.radio);
@@ -106,6 +127,8 @@ void MainWindow::on_BotonLanzar_clicked()
         s_pelotita.velY=-s_pelotita.velY;//invertimos el signo porque el eje y esta invertido
         s_pelotita.centro.setX(s_pelotita.centroI.x());
         s_pelotita.centro.setY(s_pelotita.centroI.y());
+        s_pelotita.posAntX=s_pelotita.centroI.x();
+        s_pelotita.posAntY=s_pelotita.centroI.y();
     }
 }
 
@@ -114,15 +137,36 @@ void MainWindow::RecalPelota(s_pelota *s_Pelota){
     float posAntY;
     double porcRecVel;//porcentaje de distancia de velocidad recorrida antes del choque
     float distPared;
-    posAntX=s_Pelota->centro.x();
-    posAntY=s_Pelota->centro.y();
+    //posAntX=s_Pelota->centro.x();
+    //posAntY=s_Pelota->centro.y();
 
+    posAntX=s_Pelota->posAntX;
+    posAntY=s_Pelota->posAntY;
     //calculamos el angulo de movimiento actual
     s_Pelota->angulo=qAtan(-s_Pelota->velY/s_Pelota->velX);
-
+    //con gravedad
+    if((s_Pelota->centro.y()+s_Pelota->radio)<0 && ui->Checkgravedad->isChecked()){//si la pelota no toca el piso tiene aceleracion de la gravedad
+        s_Pelota->velY=s_Pelota->velY+(s_Pelota->gravedad/100*s_Pelota->escMetro);
+    }
+    //con coeficiente de friccion
+    if((s_Pelota->centro.y()+s_Pelota->radio)==0 && ui->CheckFriccion->isChecked()){//si la pelota no toca el piso tiene aceleracion de la gravedad
+        if(s_Pelota->velX>0){
+                s_Pelota->velX=s_Pelota->velX-s_Pelota->friccion;
+                if(s_Pelota->velX<=0){//es decir si se pierde toda la energia
+                    s_Pelota->velX=0;
+                }
+        }else{
+                s_Pelota->velX=s_Pelota->velX+s_Pelota->friccion;
+                if(s_Pelota->velX>=0){//es decir si se pierde toda la energia
+                    s_Pelota->velX=0;
+                }
+        }
+    }
     //cambiamos la posicion
-    s_Pelota->centro.setX((qCeil(s_Pelota->centro.x()+s_Pelota->velX*s_Pelota->escMetro/100.0)));//se divide por 100 porque es la cantidad de veces que entran 10 ms en un segundo
-    s_Pelota->centro.setY((qCeil(s_Pelota->centro.y()+s_Pelota->velY*s_Pelota->escMetro/100.0)));
+    s_Pelota->posAntX=s_Pelota->posAntX+s_Pelota->velX*s_Pelota->escMetro/100.0;
+    s_Pelota->posAntY=s_Pelota->posAntY+s_Pelota->velY*s_Pelota->escMetro/100.0;
+    s_Pelota->centro.setX(qCeil(s_Pelota->posAntX+0.5));//se divide por 100 porque es la cantidad de veces que entran 10 ms en un segundo
+    s_Pelota->centro.setY(qCeil(s_Pelota->posAntY-0.5));
 
     //si choca la pared derecha
     if((s_Pelota->centro.x()+s_Pelota->radio)>=ui->widget->width()){
@@ -133,13 +177,14 @@ void MainWindow::RecalPelota(s_pelota *s_Pelota){
         porcRecVel=qAbs((distPared)/s_Pelota->velX);//se calcula el porcentaje de velocidad recorrido
         //ui->AnguloLineEdit->setText(QString("%1").arg(porcRecVel));
 
-        s_Pelota->velX=s_Pelota->velX-((s_Pelota->alfha/100.0)*s_Pelota->velX);//reducimos la velocidad sumando porque es negativa
+        s_Pelota->velX=s_Pelota->velX-((s_Pelota->alfha/100.0)*s_Pelota->velX);//reducimos la velocidad
         if(s_Pelota->velX>=0){//es decir si se pierde toda la energia
             s_Pelota->velX=0;
         }
         //se calcula la nueva posicion desde la pared tomando el radio y el porcentaje de velocidad que falta por recorrer luego de reducir la misma
-        if(porcRecVel>0){
-            s_Pelota->centro.setX(qCeil(ui->widget->width()-s_Pelota->radio+(s_Pelota->velX/100*s_Pelota->escMetro)*(1-porcRecVel)));
+        if(porcRecVel>0 && porcRecVel<=1){
+            s_Pelota->posAntX=(ui->widget->width()-s_Pelota->radio+(s_Pelota->velX/100*s_Pelota->escMetro)*(1-porcRecVel))-0.5;
+            s_Pelota->centro.setX(qFloor(s_Pelota->posAntX));
         }
 
 }
@@ -156,9 +201,10 @@ void MainWindow::RecalPelota(s_pelota *s_Pelota){
         s_Pelota->velX=0;
         }
         //se calcula la nueva posicion desde la pared tomando el radio y el porcentaje de velocidad que falta por recorrer luego de reducir la misma
-        if(porcRecVel>0){
+        if(porcRecVel>0 && porcRecVel<=1){
         //COLOCAMOS LA NUEVA POSICION
-        s_Pelota->centro.setX(qCeil(s_Pelota->radio+(s_Pelota->velX/100*s_Pelota->escMetro)*(1-porcRecVel)));
+        s_Pelota->posAntX=s_Pelota->radio+(s_Pelota->velX/100*s_Pelota->escMetro)*(1-porcRecVel);
+        s_Pelota->centro.setX(qCeil(s_Pelota->posAntX));
         }
 
     }
@@ -175,9 +221,10 @@ void MainWindow::RecalPelota(s_pelota *s_Pelota){
         if(s_Pelota->velY<=0){//es decir si se pierde toda la energia
         s_Pelota->velY=0;
         }
-
-        s_Pelota->centro.setY(qCeil((-ui->widget->height())+s_Pelota->radio+(s_Pelota->velY/100*s_Pelota->escMetro)*(1-porcRecVel)));
-
+        if(porcRecVel && porcRecVel<=1){
+        s_Pelota->posAntY=(-ui->widget->height())+s_Pelota->radio+(s_Pelota->velY/100*s_Pelota->escMetro)*(1-porcRecVel);
+        s_Pelota->centro.setY(qCeil(s_Pelota->posAntY));
+        }
     }
 
     //si choca el pizo
@@ -189,33 +236,22 @@ void MainWindow::RecalPelota(s_pelota *s_Pelota){
         porcRecVel=qAbs((distPared)/s_Pelota->velY);//se calcula el porcentaje de velocidad recorrido(DISTANCIA HACIA LA PARED RECORRIDA SOBRE LA VELOCIDAD)
         //ui->AnguloLineEdit->setText(QString("%1").arg(porcRecVel));
 
-        s_Pelota->velY=s_Pelota->velY-((s_Pelota->alfha/100.0)*s_Pelota->velY);//reducimos la velocidad
+        if(porcRecVel>-1){
+            s_Pelota->velY=s_Pelota->velY-((s_Pelota->alfha/100.0)*s_Pelota->velY)+(s_Pelota->gravedad/100*s_Pelota->escMetro)*(1-porcRecVel);//reducimos la velocidad y restamos tambien la gravedad recorrida calculada
+        }else{
+            s_Pelota->velY=0;
+        }
+
         if(s_Pelota->velY>=0){//es decir si se pierde toda la energia
         s_Pelota->velY=0;
         }
-        if(porcRecVel>0){
-        s_Pelota->centro.setY(qCeil((-s_Pelota->radio)+(s_Pelota->velY/100*s_Pelota->escMetro)*(1-porcRecVel)));
+        if(porcRecVel>0 && porcRecVel<=1){
+        s_Pelota->posAntY=(-s_Pelota->radio)+(s_Pelota->velY/100*s_Pelota->escMetro)*(1-porcRecVel);
+        s_Pelota->centro.setY(qCeil(s_Pelota->posAntY));
         }
        }
 
-    //con gravedad
-       if((s_Pelota->centro.y()+s_Pelota->radio)<0 && ui->Checkgravedad->isChecked()){//si la pelota no toca el piso tiene aceleracion de la gravedad
-        s_Pelota->velY=s_Pelota->velY+(s_Pelota->gravedad);
-    }
-    //con coeficiente de friccion
-    if((s_Pelota->centro.y()+s_Pelota->radio)==0 && ui->CheckFriccion->isChecked()){//si la pelota no toca el piso tiene aceleracion de la gravedad
-        if(s_Pelota->velX>0){
-        s_Pelota->velX=s_Pelota->velX-s_Pelota->friccion;
-            if(s_Pelota->velX<=0){//es decir si se pierde toda la energia
-                s_Pelota->velX=0;
-            }
-        }else{
-            s_Pelota->velX=s_Pelota->velX+s_Pelota->friccion;
-            if(s_Pelota->velX>=0){//es decir si se pierde toda la energia
-                s_Pelota->velX=0;
-            }
-        }
-    }
+
 }
 
 void MainWindow::BorrarPantalla(){
@@ -289,5 +325,68 @@ void MainWindow::on_BotonPosX_clicked()
         s_pelotita.centroI.setX(x);
     }
     BorrarPantalla();
+}
+
+void MainWindow::OnRxQSerialPort1(){
+    uint8_t cont;
+    QString str;
+    cont=QSerialPort1->bytesAvailable();
+
+    QSerialPort1->read((char*)datosLec.bufL,cont);
+    /*
+    str="";
+    for(int i=0;i<cont;i++){
+        str=str + QString(bufRx[i]);
+    }
+    */
+    ui->commandsConsole->appendPlainText(str);
+
+}
+
+
+void MainWindow::on_OpenCloseButton_clicked()
+{
+    if(QSerialPort1->isOpen()){
+        QSerialPort1->close();
+
+        ui->OpenCloseButton->setText("OPEN");
+    }else{
+        QSerialPort1->setPortName(ui->PortLinEdit->text());
+        QSerialPort1->setBaudRate(QSerialPort::Baud115200);
+
+        if(QSerialPort1->open(QSerialPort::ReadWrite)){
+        ui->OpenCloseButton->setText("CLOSE");
+        }
+    }
+}
+
+
+
+void MainWindow::on_SendButton_clicked()
+{   uint8_t ID;
+
+    char str[256];
+
+    if(QSerialPort1->isOpen()){
+        str[0]='U';
+        str[1]='N';
+        str[2]='E';
+        str[3]='R';
+        str[4]=0x02;
+        str[5]=':';
+        str[6]=0xF0;
+        str[7]=0xC4;
+        QSerialPort1->write(str,8);
+    }
+}
+
+void MainWindow::DecodeCMD()
+{
+
+}
+
+void MainWindow::ExecuteCMD(s_LDatos *datosCMD)
+{
+
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -5,6 +5,7 @@
 #include <qpaintbox.h>
 #include <QPainter>
 #include <QTimer>
+#include <QtSerialPort/QSerialPort>
 
 
 
@@ -28,6 +29,42 @@ typedef struct {
 
 }s_pelota;
 
+typedef enum{EST_U=0,EST_N,EST_E,EST_R,NUMBYTES,TOKEN,PAYLOAD,CHECKSUM}e_estCMD;
+//____________________________________________________________________________
+typedef union{
+    struct {
+        uint8_t b0:1;//se usa para saber si hay bytes leidos
+        uint8_t b1:1;
+        uint8_t b2:1;
+        uint8_t b3:1;
+        uint8_t b4:1;
+        uint8_t b5:1;
+        uint8_t b6:1;
+        uint8_t b7:1;
+    }bit;
+    uint8_t byte;
+}band;
+//______________________________________________________________________________
+typedef struct __attribute__((packed,aligned(1))){
+    uint8_t iRL;//indice de lectura
+    uint8_t iRE;//indice de escritura
+    e_estCMD estDecode;//estado del decodificado del comando
+    uint8_t nBytes;//numero de bytes de datos
+    uint8_t iDatos;//inicio de los datos en el buffer
+    uint8_t idCMD;//id del comando leido
+    uint8_t *bufL;//puntero al buffer con los datos leidos
+    uint8_t checksum;//checksum del comando
+    uint8_t iChecksum;//indice del checksum
+    uint8_t tamBuffer;//tamaño que limita el buffer debe ser de 2 a la n para que funcione correctamente la compuerta and de reinicio
+}s_LDatos;//estructura de lectura
+
+typedef struct __attribute__((packed,aligned(1))){
+    uint8_t iTL;//indice de escritura en el buffer
+    uint8_t iTE;//indice de lectura para transmitir los datos
+    uint8_t checksum;//checksum del comando
+    uint8_t *bufE;//puntero al buffer de escritura
+    uint8_t tamBuffer;//tamaño del buffer de escritura, debe ser de 2 a la n
+}s_EDatos;//estructura de escritura
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -53,13 +90,39 @@ private slots:
     void on_BotonPosY_clicked();
 
     void resizeEvent(QResizeEvent *event);
+
     void on_BotonPosX_clicked();
 
+    void OnRxQSerialPort1();
+
+    void on_OpenCloseButton_clicked();
+
+    void on_SendButton_clicked();
+
+        /** \fn DecodeCMD
+     * \brief
+     *  Esta funcion revisa si se ingresa un comando, detecta el header y una vez detectado
+     *  prepara la estructura que se utilizara para analizarlo posteriormente en otra funcion
+     * */
+        void DecodeCMD();
+
+        /** \fn ExecuteCMD
+     * \brief
+     *  Esta funcion ejecuta el comando con los datos ingresados en la estructura
+     *  una vez determinado el comando por su id procede a ejecutarlo
+     * \param[in]: datosCMD es la estructura donde estan todos los datos leidos
+     * */
+        void ExecuteCMD(s_LDatos *datosCMD);
 private:
     Ui::MainWindow *ui;
     s_pelota s_pelotita;
     QPaintBox *QPaintBall;//pantalla donde se pintara
     QTimer *TimerGen;//timer general
+    QSerialPort *QSerialPort1;
 
+    s_EDatos datosEsc;
+    uint8_t bufferE[256];
+    s_LDatos datosLec;
+    uint8_t bufferL[256];
 };
 #endif // MAINWINDOW_H
